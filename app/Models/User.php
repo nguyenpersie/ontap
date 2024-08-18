04d03a6da8fe9bf5119b35777fd7a199348508e7
Huynh Hung <hghungdev@gmail.com>
2024-08-18 18:57:12 +0700

    create seeder user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
 
-    protected $table = 'mst_users';
+    protected $table = 'users';
     protected $fillable = [
         'id',
         'name',
